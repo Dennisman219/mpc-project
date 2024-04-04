f97616a92d480f576b025ba0e350be97c2302d41
j.w.fokkink <j.w.fokkink@student.tudelft.nl>
2024-04-04 21:43:48 +0200

    hackey shit met de Q matrix

--- costgen.m
+++ costgen.m
@@ -1,6 +1,6 @@
 function [H,h,const]=costgen(P,S,Q,R,dim,x0)
 
-Qbar=kron(eye(dim.N),Q); 
+Qbar=kron(eye(dim.N),Q);
 
 H=S'*Qbar*S+kron(eye(dim.N),R);   
 h=S'*Qbar*P*x0;
--- riccati.m
+++ riccati.m
@@ -1,3 +1,6 @@
+clear all 
+close all
+
 LTI.A = [-0.0151 -60.5651 0 -32.174;
      -0.0001 -1.3411 0.9929 0;
       0.00018 43.2541 -0.86939 0;
@@ -11,13 +14,14 @@ LTI.B = [-2.516 -13.136;
 LTI.C = [0 1 0 0;
      0 0 0 1];
 
-LTI.D = [0 0;
-     0 0];
+LTI.D = [0 0; 0 0]
+
+x0 = [0;0;0;0];
 
 dim.nx = 4
 dim.ny = 2
 dim.nu = 2
-dim.N = 5
+dim.N = 2
 
 Q = eye(4)
 R = 0.1*eye(2)
@@ -30,6 +34,10 @@ Ts = 0.01;
 sysd = c2d(sys,Ts)
 
 [P, S]=predmodgen(LTI, dim)
+
+Q = eye(2)
+[H,h,const] = costgen(P, S, Q, R, dim, x0)
+
 
 % sys_setmpc = setmpcsignals(sysd);
 % mpcobj = mpc(sys_setmpc, Ts, 10);
